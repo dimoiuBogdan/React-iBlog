28f8271078ea57ad03f6ea7fec5e6622c0cc79fa
Bogdyapple14 <bogdybogdan34@gmail.com>
2021-04-15 21:05:31 +0300

    Added Click Outside Custom Hook

--- src/COMPONENTS/Navbar.js
+++ src/COMPONENTS/Navbar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import useClickOutside from "../HOOKS/useClickOutside";
 
 import SignOut from "./SignOut";
 
@@ -7,10 +7,13 @@ import HomeIcon from "@material-ui/icons/Home";
 import AccountBoxIcon from "@material-ui/icons/AccountBox";
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false);
+  const { ref, isVisible, setIsVisible } = useClickOutside(false);
 
   return (
-    <div className="z-10 container mx-auto pt-6 items-center justify-between absolute left-0 right-0 px-4 text-white flex">
+    <div
+      ref={ref}
+      className="z-10 container mx-auto pt-6 items-center justify-between absolute left-0 right-0 px-4 text-white flex"
+    >
       <div>
         <img src="" alt="" />
       </div>
@@ -18,24 +21,24 @@ const Navbar = () => {
         <div
           id="hamburger"
           className="w-10 cursor-pointer lg:hidden"
-          onClick={() => setShowMenu((prevValue) => !prevValue)}
+          onClick={() => setIsVisible((prevValue) => !prevValue)}
         >
           <div className="h-1 w-full transition duration-1000 rounded-full bg-yellow-500 my-1"></div>
           <div
             className={`h-1 transition-all delay-100 duration-700 w-4/5 ml-auto rounded-full bg-yellow-500 my-1 ${
-              showMenu ? "w-full" : ""
+              isVisible ? "w-full" : ""
             }`}
           ></div>
           <div
             className={`h-1 transition-all delay-300 duration-700 w-1/2 ml-auto rounded-full bg-yellow-500 my-1 ${
-              showMenu ? "w-full" : ""
+              isVisible ? "w-full" : ""
             }`}
           ></div>
         </div>
         <div
           className={`lg:border-0 lg:flex-row lg:bg-opacity-0 lg:relative lg:w-auto
           flex-col flex transition-all duration-1000 fixed top-0 h-full w-1/3 min-w-30rem border-r-2 border-yellow-500 left-0 bg-black bg-opacity-70 justify-evenly text-2xl items-center
-          ${showMenu ? "left-0" : "-left-96 lg:left-0"}`}
+          ${isVisible ? "left-0" : "-left-96 lg:left-0"}`}
         >
           <h3 className="mx-4 flex items-center cursor-pointer">
             Home <HomeIcon className="ml-1" />
